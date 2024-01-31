0312bfe59c0bd19a835b45542c58aa8404be7c06
LawnCable <git@lawncable.net>
2024-01-31 08:19:18 +0100

    add basic ui to create new item

--- addons/game_data_manager/ItemList.gd
+++ addons/game_data_manager/ItemList.gd
@@ -1,10 +1,13 @@
 @tool
 extends ItemList
 
+const ITEMS_FOLDER = "res://data/items"
+
 var resources: Array[Resource] = []
 
 func _ready():
-	refresh()
+	if Engine.is_editor_hint():
+		refresh()
 
 func refresh():
 	load_resources()
@@ -28,17 +31,16 @@ func showResources():
 	
 	clear()
 	for resource in filtered_resources:
-		var id = add_item(resource.name, resource.icon)
+		var id = add_item(resource.name if resource.name else "[ Unconfigured Item ] " + resource.resource_path, resource.icon)
 		set_item_tooltip(id, resource.description)
 		set_item_metadata(id, resource.resource_path)
 
 func load_resources():
 	var new_resources: Array[Resource] = []
-	var location = "res://data/items"
-	var dir = DirAccess.open(location)
+	var dir = DirAccess.open(ITEMS_FOLDER)
 	for file in dir.get_files():
 		if file.ends_with(".tres"):
-			new_resources.push_back(ResourceLoader.load(location + "/" + file))
+			new_resources.push_back(ResourceLoader.load(ITEMS_FOLDER + "/" + file))
 	resources = new_resources
 
 func _on_item_clicked(index, at_position, mouse_button_index):
@@ -47,3 +49,45 @@ func _on_item_clicked(index, at_position, mouse_button_index):
 
 func _on_line_edit_text_changed(new_text):
 	showResources()
+
+#region create item
+
+func alert(message: String):
+	$"../HBoxContainer2/AcceptDialog".dialog_text = message
+	$"../HBoxContainer2/AcceptDialog".popup_centered()
+
+func _on_create_item_pressed():
+	var textField: LineEdit = $"../HBoxContainer2/LineEdit"
+	var text: String = textField.text
+	
+	if text.is_empty():
+		alert("filename not set")
+		return
+	
+	var filename: String
+
+	if text.ends_with(".tres"):
+		filename = text
+	else:
+		filename = text + ".tres"
+	
+	var item_type = $"../HBoxContainer2/ItemTypeSelector".get_value()
+	var resource = item_type.new()
+	
+	var file_path = ITEMS_FOLDER + "/" + filename
+	if FileAccess.file_exists(file_path):
+		alert("Item exists already, choose a different filename")
+		return
+	
+	var error = ResourceSaver.save(resource, file_path)
+	if error != OK:
+		printerr("error saving resource", file_path, error)
+		alert("error saving resource, look in console for details")
+		return
+	
+	EditorInterface.edit_resource(ResourceLoader.load(file_path))
+	textField.text = ""
+	refresh()
+	$"../HBoxContainer/LineEdit".text = ""
+
+#endregion
